2488cd0b692a39c124079dc2cb095bd34215e1d4
checkdeng <checkdeng0903@gmail.com>
2020-10-22 16:21:39 +0800

    Add test with sift1M dataset

---
+++ tests/sift_test.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <vector>
+#include <set>
+#include <fstream>
+
+#include "hnsw.hpp"
+#include "bruteforce.hpp"
+#include "distance.hpp"
+
+void fvecs_read(char *filename, float *&data, uint32_t &num_vectors, int &dim);
+void ivecs_read(char *filename, int *&data, uint32_t &num_vectors, int &dim);
+
+using namespace std::chrono;
+
+using PointSet = std::vector<std::pair<uint32_t, float>>;
+
+void GenerateRandomFloat(float *data, size_t num);
+double ComputeRecall(uint32_t num, int K, const int *gt,
+                     const std::vector<PointSet> &points);
+
+int main(int argc, char **argv) {
+  int *gt_data;
+  float *base_data, *query_data;
+  uint32_t num_bases, num_queries, num_gts;
+  int dim, K;
+
+  assert(argc == 4 && "Usage: sift_test dataset_path query_path groundtruth_path");
+
+  fvecs_read(argv[1], base_data, num_bases, dim);
+  fvecs_read(argv[2], query_data, num_queries, dim);
+  ivecs_read(argv[3], gt_data, num_gts, K);
+  assert(num_gts == num_queries && "#GT must be equal to #queries");
+
+  const uint32_t M = 16;
+  const uint32_t ef = 1;
+  const uint32_t ef_construction = 32;
+
+  hnsw::L2Distance distance(dim);
+  hnsw::HNSWIndex hnsw_index(base_data, num_bases, dim, distance,
+                             M, ef, ef_construction);
+
+  // Test build
+  auto t0 = steady_clock::now();
+  hnsw_index.Build();
+  auto t1 = steady_clock::now();
+  auto duration = duration_cast<milliseconds>(t1 - t0).count();
+  printf("HNSW Build time: %ldms\n", duration);
+  printf("Top layer: %d\n", hnsw_index.TopLayer());
+  printf("Distance calculations: %ld\n", distance.num);
+
+  printf("Searching ...\n");
+  hnsw_index.SetEfConstruction(256);
+  std::vector<PointSet> result(num_queries);
+  t0 = steady_clock::now();
+  for (uint32_t i = 0; i < num_queries; i++) {
+    result[i] = hnsw_index.Search(K, &query_data[i * dim]);
+  }
+  t1 = steady_clock::now();
+  duration = duration_cast<microseconds>(t1 - t0).count();
+  printf("HNSW search speed: %ld µs/query\n", duration / num_queries);
+
+  printf("Evaluating ...\n");
+  double recall = ComputeRecall(num_queries, K, gt_data, result);
+  printf("Recall@100: %f\n", recall);
+
+  delete gt_data;
+  delete base_data;
+  delete query_data;
+
+  return 0;
+}
+
+void GenerateRandomFloat(float *data, size_t num) {
+  std::random_device rd;
+  std::mt19937 e(rd());
+  std::uniform_real_distribution<> dist(0, 10);
+
+  for (size_t i = 0; i < num; i++) {
+    data[i] = dist(e);
+  }
+}
+
+double ComputeRecall(uint32_t num, int K, const int *gt,
+                     const std::vector<PointSet> &points) {
+
+  int recalls = 0;
+  for  (size_t i = 0; i < num; i++) {
+    std::set<uint32_t> label;
+    for (int j = 0; j < K; j++) {
+      label.insert(gt[i * K + j]);
+    }
+    for (const auto &p : points[i]) {
+      if (label.count(p.first) != 0) {
+        recalls += 1;
+      }
+    }
+  }
+
+  return 1.0 * recalls / (num * K);
+}
+
+
+void fvecs_read(char *filename, float *&data, uint32_t &num_vectors, int &dim) {
+  printf("Loading data from %s\n", filename);
+  std::ifstream in(filename, std::ios::binary);
+
+  if (!in.is_open()) {
+    printf("Cannot open file: %s\n", filename);
+    exit(1);
+    return;
+  }
+
+  // Get #dimensions
+  in.read((char *)&dim, 4);
+
+  // Get file size
+  in.seekg(0, std::ios::end);
+  std::ios::pos_type ss = in.tellg();
+  size_t fsize = (size_t)ss;
+
+  // fisze = (4 + 4d) * n
+  num_vectors = fsize / (1 + dim) / 4;
+  data = new float[num_vectors * dim];
+
+  in.seekg(0, std::ios::beg);
+  for (size_t i = 0; i < num_vectors; i++) {
+    in.seekg(4, std::ios::cur);
+    in.read((char *)(data + i * dim), dim * 4);
+  }
+  in.close();
+
+  printf("Data size: (%u, %d)\n", num_vectors, dim);
+}
+
+void ivecs_read(char *filename, int *&data, uint32_t &num_vectors, int &dim) {
+  float *data_float;
+  fvecs_read(filename, data_float, num_vectors, dim);
+  data = reinterpret_cast<int *>(data_float);
+}
